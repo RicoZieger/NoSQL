3d132f019e998c0509190c4a8c1ec1020342f97b
Pascal Perle <Pascal@FamiliePerle.de>
2017-12-17 16:20:59 +0100

    updated login REST response

--- src/routes/login/login.route.ts
+++ src/routes/login/login.route.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from "express";
 import { Route } from "../../interfaces/Route";
+import { LoginResult, Message, Status, UserLevel } from "../../interfaces/Results";
 
 export class LoginRoute implements Route {
 
@@ -14,15 +15,14 @@ export class LoginRoute implements Route {
             response.setHeader('Content-Type', 'application/json');
 
             if (request.body.username != null && request.body.password != null) {
-                response.send(JSON.stringify(
-                    {
-                        status: 'SUCCESS',
-                        data: {
-                            userId: 1234567,
-                            level: 'STUDENT'
-                        }
-                    }
-                ));
+                const message: Message = {
+                    status: Status.SUCCESS,
+                    data: LoginResult
+                };
+                // TODO load real data from database
+                message.data = new LoginResult(1234567, UserLevel.STUDENT);
+
+                response.send(JSON.stringify(message));
             } else {
                 response.send(JSON.stringify(
                     {
