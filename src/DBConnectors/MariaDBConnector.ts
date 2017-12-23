7a5b6b73dc487a1234db156771fc4c4acab3a265
Pascal Perle <Pascal@FamiliePerle.de>
2017-12-23 18:11:42 +0100

    updated SQL statment prevent SQL Injection

--- src/DBConnectors/MariaDBConnector.ts
+++ src/DBConnectors/MariaDBConnector.ts
@@ -7,18 +7,23 @@ export class MariaDBConnector {
 
     public static setup() {
         MariaDBConnector.pool = mysql.createPool(process.env.MY_SQL);
-    }    
+    }
 
     public static getUserId(Id: string, passwort: string): Q.Promise<{}> {
         const deferred = Q.defer();
 
-        MariaDBConnector.pool.query('SELECT Id FROM `User` WHERE `Id` = \"' + Id + '\" AND passwort = \"' + passwort + '\";', function (error, results, fields) {
-            if(error || results.length === 0){
-                return deferred.reject();
-            }else{
-                return deferred.resolve(results[0].Id);
-            }
-        });
+        const query = 'SELECT Id FROM User WHERE User.Id = ? AND User.passwort = ? LIMIT 1;';
+        MariaDBConnector.pool
+            .getConnection((error: MysqlError, connection: PoolConnection) => {
+                connection.query(query, [Id, passwort], (error, results, fields) => {
+                    connection.release();
+                    if (error || results.length === 0) {
+                        return deferred.reject();
+                    } else {
+                        return deferred.resolve(results[0].Id);
+                    }
+                })
+            });
 
         return deferred.promise;
     }
