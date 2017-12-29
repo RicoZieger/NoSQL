92b29eda8c2312b704d37725a0751ebfd2a13e07
Pascal Perle <Pascal@FamiliePerle.de>
2017-12-29 13:02:51 +0100

    added comment for Promise import

--- src/DBConnectors/MongoDBConnector.ts
+++ src/DBConnectors/MongoDBConnector.ts
@@ -1,12 +1,12 @@
 import * as mongoose from "mongoose";
-import fs = require('fs');
-import Grid = require('gridfs-stream');
 import { IUserModel, MongoUser } from "../models/User";
 import { IThemaModel, MongoThema } from "../models/Thema";
 import { IDateiModel, MongoDatei } from "../models/Datei";
 import { IKursModel, MongoKurs } from "../models/Kurs";
 import { ITestModel, MongoTest } from "../models/Test";
-import { IFrageModel, MongoFrage} from "../models/Frage";
+import { IFrageModel, MongoFrage } from "../models/Frage";
+import fs = require('fs');
+import Grid = require('gridfs-stream');
 
 export class MongoDBConnector {
 
@@ -15,7 +15,7 @@ export class MongoDBConnector {
 
     public static setup() {
         mongoose.connect(process.env.MONGO_DB, {useMongoClient: true});
-        mongoose.Promise = require('q').Promise;
+        mongoose.Promise = require('q').Promise; // http://mongoosejs.com/docs/promises.html
         this.dbConnection = mongoose.connection;
 
         this.dbConnection
@@ -28,66 +28,67 @@ export class MongoDBConnector {
             });
     }
 
-    public static getUserByExternalId(Id: number): Promise<IUserModel>{
+    public static getUserByExternalId(Id: number): Promise<IUserModel> {
         const query = MongoUser.findOne({'Id': Id});
         const promise = query.exec();
         return promise;
     }
 
-    public static getCourseById(Id: string): Promise<IKursModel>{
+    public static getCourseById(Id: string): Promise<IKursModel> {
+        console.log(Id);
         const query = MongoKurs.findOne({'_id': Id});
         const promise = query.exec();
         return promise;
     }
 
-    public static getTestById(Id: string): Promise<ITestModel>{
+    public static getTestById(Id: string): Promise<ITestModel> {
         const query = MongoTest.findOne({'_id': Id});
         const promise = query.exec();
         return promise;
     }
 
-    public static getFileMetadataById(Id: string): Promise<IDateiModel[]>{
+    public static getFileMetadataById(Id: string): Promise<IDateiModel[]> {
         const query = MongoDatei.find({'_id': Id});
         const promise = query.exec();
         return promise;
     }
 
     public static getFileById(Id: string): any {
-        return MongoDBConnector.gfs.createReadStream({_id : Id});
+        return MongoDBConnector.gfs.createReadStream({_id: Id});
     }
 
-    public static saveFile(filename: string, filepath: string): any{
+    public static saveFile(filename: string, filepath: string): any {
         let writestream = MongoDBConnector.gfs.createWriteStream({filename: filename});
         fs.createReadStream(filepath).pipe(writestream);
         return writestream;
     }
 
-    public static saveFileWithId(id: string, filename: string, filepath: string): any{
+    public static saveFileWithId(id: string, filename: string, filepath: string): any {
         let writestream = MongoDBConnector.gfs.createWriteStream({_id: id, filename: filename});
         fs.createReadStream(filepath).pipe(writestream);
         return writestream;
     }
 
-    public static getTopicsByIds(Ids: string[]): Promise<IThemaModel[]>{
-        const query = MongoThema.find({'_id': {$in : Ids}});
+    public static getTopicsByIds(Ids: string[]): Promise<IThemaModel[]> {
+        const query = MongoThema.find({'_id': {$in: Ids}});
         const promise = query.exec();
         return promise;
     }
 
-    public static getFilesMetadataByIds(Ids: string[]): Promise<IDateiModel[]>{
-        const query = MongoDatei.find({'_id': {$in : Ids}});
+    public static getFilesMetadataByIds(Ids: string[]): Promise<IDateiModel[]> {
+        const query = MongoDatei.find({'_id': {$in: Ids}});
         const promise = query.exec();
         return promise;
     }
 
-    public static getTestsByIds(Ids: string[]): Promise<ITestModel[]>{
-        const query = MongoTest.find({'_id': {$in : Ids}});
+    public static getTestsByIds(Ids: string[]): Promise<ITestModel[]> {
+        const query = MongoTest.find({'_id': {$in: Ids}});
         const promise = query.exec();
         return promise;
     }
 
-    public static getQuestionsByIds(Ids: string[]): Promise<IFrageModel[]>{
-        const query = MongoFrage.find({'_id': {$in : Ids}});
+    public static getQuestionsByIds(Ids: string[]): Promise<IFrageModel[]> {
+        const query = MongoFrage.find({'_id': {$in: Ids}});
         const promise = query.exec();
         return promise;
     }
--- src/routes/course/course.route.ts
+++ src/routes/course/course.route.ts
@@ -1,12 +1,11 @@
 import { Route } from "../../interfaces/Route";
 import { Request, Response } from "express";
-import { IUserModel, MongoUser } from "../../models/User";
 import { IThemaModel, MongoThema } from "../../models/Thema";
 import { IDateiModel, MongoDatei } from "../../models/Datei";
 import { IKursModel, MongoKurs } from "../../models/Kurs";
 import { ITestModel, MongoTest } from "../../models/Test";
 import { IFrageModel, MongoFrage } from "../../models/Frage";
-import { CourseResult, FileMetadata, Message, QuizMetadata, Status, Topic } from "../../interfaces/Results";
+import { CourseResult, FileMetadata, QuizMetadata, Topic } from "../../interfaces/Results";
 import { MongoDBConnector } from "../../DBConnectors/MongoDBConnector";
 import filesystem = require('fs');
 
@@ -30,56 +29,56 @@ export class CourseRoute extends Route {
                 .then(CourseRoute.getAllTestsOfCourse)
                 .then(CourseRoute.getAllTopicsOfCourse)
                 .then(CourseRoute.getAllFilesOfAllCourseTopics)
-                .then(function(files){
+                .then(function (files) {
                     CourseRoute.files = files;
                     let result: CourseResult = CourseRoute.assembleCourseResult();
                     CourseRoute.sendSuccessResponse(result, response);
-                }, function(err){
+                }, function (err) {
                     CourseRoute.sendFailureResponse("Fehler beim Laden des Kurses", err, response);
                 });
         });
 
-        this.app.post('/course', (request: Request, response: Response) =>{
+        this.app.post('/course', (request: Request, response: Response) => {
 
             //expected data schema from frontend
             let data = {
                 'courseName': 'Mein neuer Kurs',
                 'courseTopics': [
                     {
-                        'topicName':'Thema1',
-                        'topicDescription':'Beschreibung zu Thema1 in meinem neuen Kurs',
-                        'files':[
+                        'topicName': 'Thema1',
+                        'topicDescription': 'Beschreibung zu Thema1 in meinem neuen Kurs',
+                        'files': [
                             {
-                                'fileName':'Wichtige Hinweise',
+                                'fileName': 'Wichtige Hinweise',
                                 'visibilityStartDate': '2016-05-18T16:00:00.000Z',
                                 'visibilityEndDate': '2018-05-18T16:00:00.000Z',
-                                'data':[
-                                    117,64,19,32,45,66,78,50
+                                'data': [
+                                    117, 64, 19, 32, 45, 66, 78, 50
                                 ]
                             }
                         ]
                     },
                     {
-                        'topicName':'Thema2',
-                        'topicDescription':'Beschreibung zu Thema2 in meinem neuen Kurs',
-                        'files':[
+                        'topicName': 'Thema2',
+                        'topicDescription': 'Beschreibung zu Thema2 in meinem neuen Kurs',
+                        'files': [
                             {
-                                'fileName':'So verdienen sie täglich 1000€',
+                                'fileName': 'So verdienen sie täglich 1000€',
                                 'visibilityStartDate': null,
                                 'visibilityEndDate': null,
-                                'data':[
-                                    117,64,19,32,45,66,78,50
+                                'data': [
+                                    117, 64, 19, 32, 45, 66, 78, 50
                                 ]
                             }
                         ]
                     }
                 ],
-                'courseQuizs':[
+                'courseQuizs': [
                     {
-                        'quizName':'Blamieren oder Kassieren (Quiz)',
+                        'quizName': 'Blamieren oder Kassieren (Quiz)',
                         'visibilityStartDate': null,
                         'visibilityEndDate': null,
-                        'questions':[
+                        'questions': [
                             {
                                 'questionText': 'Wie verdienen sie täglich 1000€?',
                                 'possibleAnwsers': [
@@ -88,8 +87,8 @@ export class CourseRoute extends Route {
                                     'es',
                                     'nicht'
                                 ],
-                                'correctAnwsers':[
-                                    2,4
+                                'correctAnwsers': [
+                                    2, 4
                                 ]
                             },
                             {
@@ -100,7 +99,7 @@ export class CourseRoute extends Route {
                                     'reich',
                                     'werden'
                                 ],
-                                'correctAnwsers':[
+                                'correctAnwsers': [
                                     3
                                 ]
                             }
@@ -109,51 +108,51 @@ export class CourseRoute extends Route {
                 ]
             }
 
-            MongoDBConnector.getCourseById('KURS_'+data.courseName)
-                .then(function(result){
-                    if(result === null){
+            MongoDBConnector.getCourseById('KURS_' + data.courseName)
+                .then(function (result) {
+                    if (result === null) {
                         //TODO Hier noch error handling? Alles zurück setzen, wenn ein Teil nicht angelegt werden kann?
                         let newCourse = CourseRoute.createCourseRecursively(data);
                         newCourse.save();
                         CourseRoute.sendSuccessResponse("Kurs angelegt", response);
-                    }else{
+                    } else {
                         CourseRoute.sendFailureResponse("Ein Kurs mit diesem Namen existiert bereits", null, response);
                     }
-                }, function(err){
+                }, function (err) {
                     CourseRoute.sendFailureResponse("Fehler beim Abfragen der bestehenden Kurse", err, response);
                 });
         });
     }
 
-    private static createCourseRecursively(courseData:any): IKursModel{        
+    private static createCourseRecursively(courseData: any): IKursModel {
         let newCourse = new MongoKurs();
         let topics: any[] = courseData.courseTopics;
         let quizs: any[] = courseData.courseQuizs;
 
-        newCourse._id = 'KURS_'+courseData.courseName;
+        newCourse._id = 'KURS_' + courseData.courseName;
         newCourse.Titel = courseData.courseName;
         newCourse.Themen = [];
         newCourse.Tests = [];
 
-        for(let i = 0; i < topics.length; i++){
+        for (let i = 0; i < topics.length; i++) {
             newCourse.Themen.push(CourseRoute.createTopicRecursively(topics[i], newCourse._id)._id);
         }
-        for(let i = 0; i < quizs.length; i++){
+        for (let i = 0; i < quizs.length; i++) {
             newCourse.Tests.push(CourseRoute.createQuizRecursively(quizs[i], newCourse._id)._id);
         }
 
         return newCourse;
     }
 
-    private static createTopicRecursively(topicData: any, idPrefix: string): IThemaModel{
+    private static createTopicRecursively(topicData: any, idPrefix: string): IThemaModel {
         let newTopic = new MongoThema();
         let files: any[] = topicData.files;
 
-        newTopic._id = idPrefix+'_'+topicData.topicName;
+        newTopic._id = idPrefix + '_' + topicData.topicName;
         newTopic.Titel = topicData.topicName;
         newTopic.Text = topicData.topicDescription;
         newTopic.Dateien = [];
-        for(let i = 0; i < topicData.files.length; i++){
+        for (let i = 0; i < topicData.files.length; i++) {
             newTopic.Dateien.push(CourseRoute.createFileRecursively(topicData.files[i], newTopic._id)._id);
         }
 
@@ -161,10 +160,10 @@ export class CourseRoute extends Route {
         return newTopic;
     }
 
-    private static createFileRecursively(fileData: any, idPrefix: string):IDateiModel{
+    private static createFileRecursively(fileData: any, idPrefix: string): IDateiModel {
         let newFile = new MongoDatei();
 
-        newFile._id = idPrefix+'_'+fileData.fileName;
+        newFile._id = idPrefix + '_' + fileData.fileName;
         newFile.Titel = fileData.fileName;
 
         fileData.visibilityStartDate === null ? newFile.Anfangsdatum = null :
@@ -177,11 +176,11 @@ export class CourseRoute extends Route {
         return newFile;
     }
 
-    private static saveFile(file: any, filename: string, idPrefix: string): string{
-        const tmpFileName = "tmp_up_"+filename;
-        const id = idPrefix+'_ActualData';
+    private static saveFile(file: any, filename: string, idPrefix: string): string {
+        const tmpFileName = "tmp_up_" + filename;
+        const id = idPrefix + '_ActualData';
 
-        filesystem.writeFile(tmpFileName, Buffer.from(file), function(err){
+        filesystem.writeFile(tmpFileName, Buffer.from(file), function (err) {
             let fileStream = MongoDBConnector.saveFileWithId(id, filename, tmpFileName);
             fileStream.on('close', function (file) {
                 filesystem.unlinkSync(tmpFileName);
@@ -191,10 +190,10 @@ export class CourseRoute extends Route {
         return id;
     }
 
-    private static createQuizRecursively(quizData: any, idPrefix: string): ITestModel{
+    private static createQuizRecursively(quizData: any, idPrefix: string): ITestModel {
         let newQuiz = new MongoTest();
 
-        newQuiz._id = idPrefix+'_'+quizData.quizName;
+        newQuiz._id = idPrefix + '_' + quizData.quizName;
         newQuiz.Titel = quizData.quizName;
         quizData.visibilityStartDate === null ? newQuiz.Anfangsdatum = null :
             newQuiz.Anfangsdatum = new Date(quizData.visibilityStartDate);
@@ -202,15 +201,15 @@ export class CourseRoute extends Route {
             newQuiz.Ablaufdatum = new Date(quizData.visibilityEndDate);
         newQuiz.AbgeschlossenVon = [];
         newQuiz.Fragen = [];
-        for(let i = 0; i < quizData.questions.length; i++){
-            newQuiz.Fragen.push(CourseRoute.createQuestionRecursively(quizData.questions[i], (newQuiz._id+'_Frage'+i))._id);
+        for (let i = 0; i < quizData.questions.length; i++) {
+            newQuiz.Fragen.push(CourseRoute.createQuestionRecursively(quizData.questions[i], (newQuiz._id + '_Frage' + i))._id);
         }
 
         newQuiz.save();
         return newQuiz;
     }
 
-    private static createQuestionRecursively(questionData: any, id: string):IFrageModel{
+    private static createQuestionRecursively(questionData: any, id: string): IFrageModel {
         let newQuestion = new MongoFrage();
 
         newQuestion._id = id;
@@ -222,7 +221,7 @@ export class CourseRoute extends Route {
         return newQuestion;
     }
 
-    private static assembleCourseResult(): CourseResult{
+    private static assembleCourseResult(): CourseResult {
         const courseResult: CourseResult = new CourseResult(CourseRoute.course._id, CourseRoute.course.Titel);
         const courseTopics: Topic[] = new Array(CourseRoute.topics.length);
         const courseFiles: FileMetadata[] = [];
@@ -230,16 +229,16 @@ export class CourseRoute extends Route {
         let now: Date = new Date();
 
         for (let quizCounter = 0; quizCounter < CourseRoute.tests.length; quizCounter++) {
-            if(CourseRoute.isFileInVisibleNow(CourseRoute.tests[quizCounter].Anfangsdatum,
-                CourseRoute.tests[quizCounter].Ablaufdatum)){
+            if (CourseRoute.isFileInVisibleNow(CourseRoute.tests[quizCounter].Anfangsdatum,
+                    CourseRoute.tests[quizCounter].Ablaufdatum)) {
                 courseTests.push(new QuizMetadata(CourseRoute.tests[quizCounter]._id,
                     CourseRoute.tests[quizCounter].Titel));
             }
         }
 
         for (let fileCounter = 0; fileCounter < CourseRoute.files.length; fileCounter++) {
-            if(CourseRoute.isFileInVisibleNow(CourseRoute.files[fileCounter].Anfangsdatum,
-                CourseRoute.files[fileCounter].Ablaufdatum)){
+            if (CourseRoute.isFileInVisibleNow(CourseRoute.files[fileCounter].Anfangsdatum,
+                    CourseRoute.files[fileCounter].Ablaufdatum)) {
                 courseFiles.push(new FileMetadata(CourseRoute.files[fileCounter]._id,
                     CourseRoute.files[fileCounter].Titel, "FileLinkNotExistingYet"));
             }
@@ -270,29 +269,29 @@ export class CourseRoute extends Route {
         return courseResult;
     }
 
-    private static isFileInVisibleNow(visibilityStart: Date, visibilityEnd: Date): boolean{
+    private static isFileInVisibleNow(visibilityStart: Date, visibilityEnd: Date): boolean {
         let now: Date = new Date();
 
         return ((visibilityStart === null && visibilityEnd === null) ||
-        ((visibilityStart <= now) && (visibilityEnd >= now)));
+            ((visibilityStart <= now) && (visibilityEnd >= now)));
     }
 
-    private static getAllTestsOfCourse(course: IKursModel): Promise<ITestModel[]>{
+    private static getAllTestsOfCourse(course: IKursModel): Promise<ITestModel[]> {
         CourseRoute.course = course;
         return MongoDBConnector.getTestsByIds(course.Tests);
     }
 
-    private static getAllTopicsOfCourse(tests: ITestModel[]): Promise<IThemaModel[]>{
+    private static getAllTopicsOfCourse(tests: ITestModel[]): Promise<IThemaModel[]> {
         CourseRoute.tests = tests;
         return MongoDBConnector.getTopicsByIds(CourseRoute.course.Themen);
     }
 
-    private static getAllFilesOfAllCourseTopics(topics: IThemaModel[]): Promise<IDateiModel[]>{
+    private static getAllFilesOfAllCourseTopics(topics: IThemaModel[]): Promise<IDateiModel[]> {
         CourseRoute.topics = topics;
-        let fileIds: string[] = new Array();
+        let fileIds: string[] = [];
 
-        for(let i = 0; i < topics.length; i++){
-            for(let f = 0; f < topics[i].Dateien.length; f++){
+        for (let i = 0; i < topics.length; i++) {
+            for (let f = 0; f < topics[i].Dateien.length; f++) {
                 fileIds.push(topics[i].Dateien[f]);
             }
         }
