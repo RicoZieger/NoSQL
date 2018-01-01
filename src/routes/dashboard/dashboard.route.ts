90a0b64001f7a8970f97c6c8a4d300e10300e545
RicoZieger <rico.zieger@gmail.com>
2018-01-02 00:05:42 +0100

    refactored dashboard route to reduce level of nested method calls

--- src/routes/dashboard/dashboard.route.ts
+++ src/routes/dashboard/dashboard.route.ts
@@ -2,37 +2,45 @@ import { Request, Response } from "express";
 import { Route } from "../../interfaces/Route";
 import { MongoDBConnector } from "../../DBConnectors/MongoDBConnector";
 import { QuizUserResult } from "../../interfaces/Results";
+import { IUserModel } from "../../models/User";
+import { ITestergebnisModel } from "../../models/Testergebnis";
 
 export class DashboardRoute extends Route {
 
     public getRoutes(): void {
-
         //returns the test results for all students in the given course/quiz)
         //TODO prüfen ob der angegebene Nutzer die Berechtigung hat
         this.app.get('/dashboards/users/:userId/courses/:courseId/quizs/:quizId', (request: Request, response: Response) => {
+            let testresults: ITestergebnisModel[] = [];
+
             MongoDBConnector.getAllTestresultsOfTest(request.params.quizId)
-                .then(function(testresults){
-                    MongoDBConnector.getAllStudentsOfCourse(request.params.courseId)
-                        .then(function(students){
-                            let result: QuizUserResult[] = [];
-
-                            for(let i = 0; i < students.length; i++){
-                                let tmp = new QuizUserResult(students[i]._id, "Nicht teilgenommen");
-                                for(let t = 0; t < testresults.length; t++){
-                                    if(testresults[t].ZugehörigerUser === students[i]._id){
-                                        tmp.points = testresults[t].ErreichtePunkte+" Punkte";
-                                    }
-                                }
-                                result.push(tmp);
-                            }
-
-                            DashboardRoute.sendSuccessResponse(result, response);
-                        }, function(err){
-                            DashboardRoute.sendFailureResponse("Fehler beim Laden der Testergebnisse", err, response);
-                        });
+                .then(function(value){
+                    testresults = value;
+                    return request.params.courseId;
+                })
+                .then(MongoDBConnector.getAllStudentsOfCourse)
+                .then(function(students){
+                    DashboardRoute.sendSuccessResponse(DashboardRoute.assembleQuizUserResult(students, testresults), response);
                 }, function(err){
                     DashboardRoute.sendFailureResponse("Fehler beim Laden der Testergebnisse", err, response);
                 });
         });
     }
+
+    private static assembleQuizUserResult(students: IUserModel[], testresults: ITestergebnisModel[]): QuizUserResult[]{
+        let result: QuizUserResult[] = [];
+
+        for(let i = 0; i < students.length; i++){
+            let tmp = new QuizUserResult(students[i]._id, "Nicht teilgenommen");
+            for(let t = 0; t < testresults.length; t++){
+                if(testresults[t].ZugehörigerUser === students[i]._id){
+                    tmp.points = testresults[t].ErreichtePunkte+" Punkte";
+                }
+            }
+            result.push(tmp);
+        }
+
+        return result;
+    }
+
 }
