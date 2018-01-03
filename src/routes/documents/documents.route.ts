284a413db3baad7e5b49833b5a5cb49cc133e4a3
RicoZieger <rico.zieger@gmail.com>
2018-01-03 17:40:44 +0100

    Updated experimental file download route

--- src/routes/documents/documents.route.ts
+++ src/routes/documents/documents.route.ts
@@ -8,48 +8,58 @@ import filesystem = require('fs');
 /*
     NOTE
     This class exists only for test purposes and might not be consistent when it comes to coding and/or naming
-    conventions. 
+    conventions.
 */
 export class DocumentRoute extends Route {
 
     getRoutes(): void {
-        //TODO Nur Studenten den Download gestatten, die in dem zu den Dateien zugehörigen Kurs eingeschrieben sind
-        this.app.get('/file/:fileId', (request: Request, response: Response) => {
+        //NOTE Nur Studenten, die in dem zu den Dateien zugehörigen Kurs eingeschrieben sind, sind berechtigt
+        this.app.get('users/:userId/courses/:courseId/files/:fileId', (request: Request, response: Response) => {
 
-            //TODO Dateiname aus Metadaten einlesen
-            let mimetype = mime.lookup("TestDatei.pdf");
-            response.setHeader('Content-type', mimetype);
+            DocumentRoute.isTokenValid(request.params.userId, request.header('request-token'))
+            .then(function(isValid){
+                return request.params.userId;
+            })
+            .then(DocumentRoute.isUserStudent)
+            .then(function(isStudent){
+                return request.params.fileId;
+            })
+            .then(MongoDBConnector.getFileMetadataById)
+            .then(function(file){
+                const fileStream = MongoDBConnector.getFileById(file.gridfsLink);
+                const tmpFileName = 'tmp_down_'+file.gridfsLink;
+                let mimetype = mime.lookup(file.Titel);
+                let data = [];
+                response.setHeader('Content-type', mimetype);
 
-            const fileId = request.params.fileId;
-            const fileStream = MongoDBConnector.getFileById(fileId);
-            const tmpFileName = 'tmp_down_'+fileId;
-            let data = [];
-
-            fileStream.on('error', function (err) {
-                DocumentRoute.sendFailureResponse("Datei konnte nicht heruntergeladen werden", err, response);
-            });
-
-            fileStream.on('data', function(chunk){
-                data.push(chunk);
-            });
-
-            fileStream.on('end', function(){
-                filesystem.writeFile(tmpFileName, Buffer.concat(data), function(err){
-                    if(err){
-                        DocumentRoute.sendFailureResponse("Datei konnte zur Weiterverarbeitung nicht zwischengespeichert werden", err, response);
-                    }else{
-                        response.download(tmpFileName, "Dateiname_aus_Metadaten.pdf", function(err){
-                            if(err){
-                                DocumentRoute.sendFailureResponse("Fehler beim Herunterladen der Datei", err, response);
-                            }
-                            filesystem.unlinkSync(tmpFileName);
-                        });
-                    }
+                fileStream.on('error', function (err) {
+                    DocumentRoute.sendFailureResponse("Datei konnte nicht heruntergeladen werden", err, response);
                 });
+
+                fileStream.on('data', function(chunk){
+                    data.push(chunk);
+                });
+
+                fileStream.on('end', function(){
+                    filesystem.writeFile(tmpFileName, Buffer.concat(data), function(err){
+                        if(err){
+                            DocumentRoute.sendFailureResponse("Datei konnte zur Weiterverarbeitung nicht zwischengespeichert werden", err, response);
+                        }else{
+                            response.download(tmpFileName, "Dateiname_aus_Metadaten.pdf", function(err){
+                                if(err){
+                                    DocumentRoute.sendFailureResponse("Fehler beim Herunterladen der Datei", err, response);
+                                }
+                                filesystem.unlinkSync(tmpFileName);
+                            });
+                        }
+                    });
+                });
+            }, function(err){
+                DocumentRoute.sendFailureResponse("Herunterladen der Datei fehlgeschlagen", err, response);
             });
         });
 
-        this.app.post('/file/:filename', (request: Request, response: Response) => {            
+        this.app.post('/file/:filename', (request: Request, response: Response) => {
             const upload = request.body;
             const filename = request.params.filename;
             const tmpFileName = "tmp_up_"+filename;
