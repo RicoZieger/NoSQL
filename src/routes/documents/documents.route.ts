eb7f27705011cf7c466e9c4d815a4fb7989a52dc
RicoZieger <rico.zieger@gmail.com>
2018-01-06 21:24:00 +0100

    Removed old file upload route

--- src/routes/documents/documents.route.ts
+++ src/routes/documents/documents.route.ts
@@ -9,7 +9,7 @@ export class DocumentRoute extends Route {
     getRoutes(): void {
         //NOTE Nur Nutzer, die in dem zu den Dateien zugehörigen Kurs eingeschrieben sind, sind berechtigt
         //NOTE Da der Download im Frontend nur über ein href möglich ist, muss der token in der URL übergeben werden
-        this.app.get('/users/:userId/courses/:courseId/files/:fileId/token/:tokenId', (request: Request, response: Response) => {            
+        this.app.get('/users/:userId/courses/:courseId/files/:fileId/token/:tokenId', (request: Request, response: Response) => {
             DocumentRoute.hasUserAccessToFile(request.params.userId, request.params.courseId, request.params.tokenId)
             .then(function(hasAccess){
                 return request.params.fileId;
@@ -47,41 +47,7 @@ export class DocumentRoute extends Route {
             }, function(err){
                 DocumentRoute.sendFailureResponse("Herunterladen der Datei fehlgeschlagen", err, response);
             });
-        });
-
-        /*
-            NOTE
-            This method exists only for test purposes and might not be consistent when it comes to coding and/or naming
-            conventions.
-        */
-        this.app.post('/file/:filename', (request: Request, response: Response) => {
-            const upload = request.body;
-            const filename = request.params.filename;
-            const tmpFileName = "tmp_up_"+filename;
-            let data = [];
-
-            request.on('data', function(chunk){
-                data.push(chunk);
-            });
-
-            request.on('end', function(){
-                filesystem.writeFile(tmpFileName, Buffer.concat(data), function(err){
-                    if(err){
-                        DocumentRoute.sendFailureResponse("Datei konnte zur Weiterverarbeitung nicht zwischengespeichert werden", err, response);
-                    }else{
-                        let fileStream = MongoDBConnector.saveFile(filename, tmpFileName);
-
-                        fileStream.on('error', function (err) {
-                            DocumentRoute.sendFailureResponse("Datei konnte nicht gespeichert werden", err, response);
-                        });
-                        fileStream.on('close', function (file) {
-                            DocumentRoute.sendSuccessResponse("Datei wurde gespeichert unter der id: "+file._id, response);
-                            filesystem.unlinkSync(tmpFileName);
-                        });
-                    }
-                });
-            });
-        });
+        });        
     }
 
     private static hasUserAccessToFile(userId: string, courseId: string, token: string): Promise<boolean> {
